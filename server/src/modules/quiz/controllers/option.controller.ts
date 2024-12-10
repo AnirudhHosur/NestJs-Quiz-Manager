01285e9715234ddf7df041b372159ead13d8e902
Anirudh Hosur <53824669+AnirudhHosur@users.noreply.github.com>
2024-12-10 15:39:46 -0700

    CRUD operations for option entity

--- server/src/modules/quiz/controllers/option.controller.ts
+++ server/src/modules/quiz/controllers/option.controller.ts
@@ -1,18 +1,42 @@
-import { Body, Controller, Post, UsePipes, ValidationPipe } from "@nestjs/common";
+import { Body, Controller, Get, Param, ParseIntPipe, Post, Put, UsePipes, ValidationPipe } from "@nestjs/common";
+import { CreateOptionDto } from "../dto/create-option.dto";
+import { UpdateOptionDto } from "../dto/update-option.dto";
+import { Option } from "../entities/option.entity";
 import { OptionService } from "../services/option.service";
 import { QuestionService } from "../services/question.service";
-import { CreateOptionDto } from "../dto/create-option.dto";
-import { Option } from "../entities/option.entity";
 
 @Controller('question/option')
 export class OptionController {
 
     constructor(private optionService: OptionService, private questionService: QuestionService) { }
 
-    @Post('')
+    // Get all options
+    @Get('/')
+    async getAllOptions(): Promise<Option[]> {
+        return await this.optionService.getAllOptions();
+    }
+
+    // Get option by id
+    @Get('/:id')
+    async getOptionById(@Param('id', ParseIntPipe) id: number): Promise<Option> {
+        return await this.optionService.getOptionById(id);
+    }
+
+    @Post('/create')
     @UsePipes(ValidationPipe)
     async saveOptionToQuestion(@Body() createOption: CreateOptionDto): Promise<Option> {
         const question = await this.questionService.getQuestionById(createOption.questionId);
         return await this.optionService.createOption(createOption, question);
     }
+
+    @Put('/:id')
+    @UsePipes(ValidationPipe)
+    async updateOption(
+        @Param('id', ParseIntPipe) id: number,
+        @Body() updateOptionDto: UpdateOptionDto
+    ): Promise<Option> {
+        return await this.optionService.updateOption(id, updateOptionDto);
+    }
+
+
 }
--- server/src/modules/quiz/services/option.service.ts
+++ server/src/modules/quiz/services/option.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { OptionRepository } from "../repositories/option.repository";
 import { CreateOptionDto } from "../dto/create-option.dto";
-import { Question } from "../entities/question.entity";
 import { Option } from "../entities/option.entity";
+import { Question } from "../entities/question.entity";
+import { OptionRepository } from "../repositories/option.repository";
+import { UpdateOptionDto } from "../dto/update-option.dto";
 
 @Injectable()
 export class OptionService {
@@ -11,7 +12,27 @@ export class OptionService {
     constructor(
         @InjectRepository(Option)
         private optionRepo: OptionRepository,
-    ) {}
+    ) { }
+
+    // Get all options
+    async getAllOptions(): Promise<Option[]> {
+        return await this.optionRepo.find({
+            relations: ['question'],
+        });
+    }
+
+    // Get an option by ID
+    async getOptionById(id: number): Promise<Option> {
+        const option = await this.optionRepo.findOne({
+            where: { id },
+            relations: ['question'],
+        });
+        if (!option) {
+            throw new NotFoundException(`Option with ID ${id} not found`);
+        }
+        return option;
+    }
+
 
     async createOption(option: CreateOptionDto, question: Question): Promise<Option> {
         const newOption = this.optionRepo.create({
@@ -19,5 +40,19 @@ export class OptionService {
             question
         })
         return await this.optionRepo.save(newOption);
+    }
+
+    // Update an option
+    async updateOption(id: number, updateOptionDto: UpdateOptionDto): Promise<Option> {
+        const option = await this.getOptionById(id);
+        Object.assign(option, updateOptionDto);
+        return await this.optionRepo.save(option);
+    }
+
+    // Delete an option
+    async deleteOption(id: number): Promise<{ message: string }> {
+        const option = await this.getOptionById(id);
+        await this.optionRepo.remove(option);
+        return { message: `Option with ID ${id} successfully deleted` };
     }
 }
